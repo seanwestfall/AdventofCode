500365b9e6c4657f16e0ede7a1181e1bd7d55c7e
seanwestfall <sean.westfall@gmail.com>
2020-06-30 04:18:44 -0700

    sw / update to 03

--- 03/03.hs
+++ 03/03.hs
@@ -2,4 +2,9 @@
 main :: IO ()
 main = do
     content <- readFile "03.txt"
+    -- create the manhattan distance
+    -- find where the wires cross
+    -- compute the distance
+    let n = [(x, y)| x<- [0..10], y<-[0..10]]
     putStrLn $ content
+    mapM_ print n
