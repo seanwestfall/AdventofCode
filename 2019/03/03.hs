8cba38f4f90bf55fc1045af3a7c82151dd4286d9
seanwestfall <sean.westfall@gmail.com>
2021-09-11 10:09:11 -0700

    added some code for printing a matrix

--- 2019/03/03.hs
+++ 2019/03/03.hs
@@ -1,3 +1,7 @@
+import Data.List (transpose)
+import Data.List (intercalate)
+
+showMat = unTable " " . equalizeCellLengths . (map . map) show
 
 main :: IO ()
 main = do
@@ -6,5 +10,50 @@ main = do
     -- find where the wires cross
     -- compute the distance
     let n = [(x, y)| x<- [0..10], y<-[0..10]]
-    putStrLn $ content
-    mapM_ print n
+    -- putStrLn $ content
+    putStrLn $ showMat [[0,0,0],[0,1,0],[0,1,0]]
+
+
+-- | Un-tabs, and intersperses the result with \\n. This converts a 2-dimensional
+--   list of strings into a string where ends of a line are \\n, and the
+--   individual cells are divided by \\t.
+unTable :: String     -- ^ Column spacer
+        -> [[String]] -- ^ Table
+        -> String
+unTable colSpacer = intercalate "\n" . map (intercalate colSpacer)
+
+
+
+-- | Finds the length of the longest line of a table
+maxLineLength :: [[a]] -> Int
+maxLineLength = maximum . map length
+
+
+
+-- | Calculates the length of the longest entry of each column.
+maxCellLengthsPerColumn :: [[[a]]] -> [Int]
+maxCellLengthsPerColumn = map maxLineLength . transpose
+
+
+
+-- | Reformats the table.
+equalizeCellLengths :: [[String]] -> [[String]]
+equalizeCellLengths table = map (equalize maxLengths) table
+        where
+                maxLengths = maxCellLengthsPerColumn table
+
+                -- equalize takes a list of lengths and a 2-dimensional table, and
+                -- pads the columns so all entries have the same length.
+                equalize = flip $ zipWith (padToLengthLeft ' ')
+
+
+
+-- | Pads a list to a certain length on the right.
+--
+--   >>> padToLengthRight '.' "abc" 5
+--   "abc.."
+padToLengthLeft :: a   -- ^ Padding element
+                -> [a] -- ^ List to be padded
+                -> Int -- ^ Length to pad to
+                -> [a]
+padToLengthLeft padding list n = (replicate (n - length list) padding) ++ list
